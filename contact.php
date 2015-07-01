d3f437c64521832e47f994b65dad60b813ed7c02
Luke Fallon <lukef94@gmail.com>
2015-07-01 15:16:06 -0400

    Added basic site files, logo image, header

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Home - AKC Services</title>
+	<title>Contact - AKC Services</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
